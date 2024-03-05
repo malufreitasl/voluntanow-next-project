34007102d8889a66f0959fcf71e17876d1b27cc0
Gabriela Nunes <gabrielassnunes@gmail.com>
2024-03-05 12:27:43 +0000

    about-us

---
+++ src/pages/about-us/main.jsx
@@ -0,0 +1,19 @@
+import { NavBar } from '../components/NavBar'
+import { Footer } from "../components/Footer"
+
+
+
+export default function AboutUs(){
+    return(
+        <>
+        <NavBar/>
+        <div className='pt-64 px-6 w-full'>
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 
+            <h1>Sobre Nós</h1>
+        </div>
+        
+        
+        <Footer/>
+        </>
+    )
+}
